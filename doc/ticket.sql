253de31e81b026865e4ac18891ed47a8bdb9ff23
liuhailong <liuhailong@55haitao.com>
2017-10-25 17:23:36 +0800

    添加sql语句文件

---
+++ doc/ticket.sql
@@ -0,0 +1,45 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.7.17-log : Database - ticket
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ticket` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ticket`;
+
+/*Table structure for table `tickets` */
+
+DROP TABLE IF EXISTS `tickets`;
+
+CREATE TABLE `tickets` (
+  `id` bigint(19) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `period` varchar(8) DEFAULT NULL COMMENT '期数',
+  `red1` int(2) DEFAULT NULL COMMENT '红球1',
+  `red2` int(2) DEFAULT NULL,
+  `red3` int(2) DEFAULT NULL,
+  `red4` int(2) DEFAULT NULL,
+  `red5` int(2) DEFAULT NULL,
+  `red6` int(2) DEFAULT NULL,
+  `blue1` int(2) DEFAULT NULL COMMENT '蓝球',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tickets` */
+
+insert  into `tickets`(`id`,`period`,`red1`,`red2`,`red3`,`red4`,`red5`,`red6`,`blue1`,`create_time`,`update_time`) values (1,'17125',1,14,23,25,29,30,3,'2017-10-25 14:58:53','2017-10-25 00:00:00');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
