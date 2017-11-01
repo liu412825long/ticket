e424de65841f24be5fcdbcdb49c78bdfa63d514c
liuhailong <liuhailong@55haitao.com>
2017-11-01 19:40:43 +0800

    fix

--- src/main/java/com/dragon/ticket/controller/TestController.java
+++ src/main/java/com/dragon/ticket/controller/TestController.java
@@ -1,17 +1,8 @@
 package com.dragon.ticket.controller;
 
-import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.dragon.ticket.model.Tickets;
-import com.dragon.ticket.service.TicketsService;
 
 @Controller
 public class TestController {
